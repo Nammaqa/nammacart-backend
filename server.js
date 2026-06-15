5d6fb1b065c49c926adfd6215407bfe8473177ed
NammaQA <hello@nammaqa.com>
2026-06-15 11:50:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: '*' }));
 app.use(fileUpload({ createParentPath: true, parseNested: true }))
 
 // normalize duplicate slashes in request URLs
